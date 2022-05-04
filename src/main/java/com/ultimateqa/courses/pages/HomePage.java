69b9509761f01328f9a6b02579f2c456631717a5
Vimal Patel <vpatel4496@gmail.com>
2022-05-04 22:50:18 +0100

    ultimateqa-p1

---
+++ src/main/java/com/ultimateqa/courses/pages/HomePage.java
@@ -0,0 +1,17 @@
+package com.ultimateqa.courses.pages;
+
+import com.ultimateqa.courses.utilities.Utility;
+import org.openqa.selenium.By;
+
+public class HomePage extends Utility {
+
+    By sign= By.linkText("Sign In");
+    By title=By.xpath("//h1[@class='page__heading']");
+
+    public void clickOnLoginBtn(){
+        clickOnElement(sign);
+    }
+    public String verifyWelcomeText(){
+        return getTextFromElement(title);
+    }
+}
